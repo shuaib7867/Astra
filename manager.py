7e925b168d3f6774a66088813af88599791b0c67
Patel Harsh <harshpatelbbu1995@gmail.com>
2021-07-23 13:39:40 +0530

    Update manager.py

--- manager.py
+++ manager.py
@@ -34,7 +34,7 @@ def banner():
     for char in b:
         print(f'{random.choice(colors)}{char}{n}')
     #print('=============SON OF GENISYS==============')
-    print(f'   Version: 1.2 | Author: Cryptonian{n}\n')
+    print(f'   Version: 1.2 | Author: ʜᴀʀsʜ & ᴀʟᴄᴏʜᴏʟɪᴄ{n}\n')
 
 def clr():
     if os.name == 'nt':
@@ -143,8 +143,8 @@ while True:
         # thanks to github.com/th3unkn0n for the snippet below
         print(f'\n{lg}[i] Checking for updates...')
         try:
-            # https://raw.githubusercontent.com/Cryptonian007/Astra/main/version.txt
-            version = requests.get('https://raw.githubusercontent.com/Cryptonian007/Astra/main/version.txt')
+            # https://raw.githubusercontent.com/Harsh-78/Astra/main/version.txt
+            version = requests.get('https://raw.githubusercontent.com/Harsh-78/Astra/main/version.txt')
         except:
             print(f'{r} You are not connected to the internet')
             print(f'{r} Please connect to the internet and retry')
@@ -160,8 +160,8 @@ while True:
                     os.system('rm add.py')
                     os.system('rm manager.py')
                 #os.system('del scraper.py')
-                os.system('curl -l -O https://raw.githubusercontent.com/Cryptonian007/Astra/main/add.py')
-                os.system('curl -l -O https://raw.githubusercontent.com/Cryptonian007/Astra/main/manager.py')
+                os.system('curl -l -O https://raw.githubusercontent.com/Harsh-78/Astra/main/add.py')
+                os.system('curl -l -O https://raw.githubusercontent.com/Harsh-78/Astra/main/manager.py')
                 print(f'{lg}[*] Updated to version: {version.text}')
                 input('Press enter to exit...')
                 exit()
